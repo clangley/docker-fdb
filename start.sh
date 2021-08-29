2c2694990b61208a9d2720fcac08036289faf77f
Chris Langley <chriswlangley@outlook.com>
2021-08-29 09:48:04 -0500

    github workspace

--- start.sh
+++ start.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
-if [ ! -f /var/lib/foundationdb/fdb.cluster ]
+#cp /etc/fdb/foundationdb.conf /etc/foundationdb/foundationdb.conf
+export FDB_IP=$(python3 get_ip.py)
+cat /etc/fdb/foundationdb.conf | envsubst "\$FDB_IP" > /etc/foundationdb/foundationdb.conf
+if [ ! -f /etc/foundationdb/fdb.cluster ]
 then
     if [ "$1" = "" ]
     then
         echo "Configure new cluster."
-        /usr/lib/foundationdb/make_public.py
-        cp /etc/foundationdb/fdb.cluster /var/lib/foundationdb/fdb.cluster
-        (sleep 1 && fdbcli --no-status --exec "configure new single ssd" &)
+        echo "docker:docker@$FDB_IP:4500" > /etc/foundationdb/fdb.cluster
+        (sleep 3 && fdbcli --no-status --exec "configure new single ssd" &)
     else
         echo "Replace $FDB_CLUSTER_FILE with $1 to join."
-        echo $1 > /var/lib/foundationdb/fdb.cluster
+        echo $1 > /etc/foundationdb/fdb.cluster
     fi
 fi
 exec /usr/lib/foundationdb/fdbmonitor
